9423e6bbdc1fa6cf871815bba34b2f2baa27f360
RelaxSource <78817481+RelaxSource@users.noreply.github.com>
2021-02-11 14:59:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ chmod +x ts
 get() {
 rm -fr Relax.lua
 rm -fr sudo.lua
-wget "https://raw.githubusercontent.com/TshAkEAb/TshAkE/master/Tshake.lua"
+wget "https://raw.githubusercontent.com/RelaxSource/Relax/main/Relax.lua"
 lua start.lua
 }
 installall(){
